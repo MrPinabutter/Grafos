12837341e2919223ec7da3db98370646171609b7
MrPinabutter <vitorafael.r.c@gmail.com>
2020-12-01 23:01:32 -0300

    Verificando regularidade

--- grafo_completo.py
+++ grafo_completo.py
@@ -20,4 +20,24 @@ def isConectado(grafo, vertice, visitados):
     return len(visitados) == len(grafo)
 print('O grafo é conectado' if isConectado(grafo, 0, []) else 'O grafo não é conectado')
 
-    
+def isRegular(grafo):
+    val = 0
+    if len(grafo) == 1:
+        return True
+    for i, vizinhos in enumerate(grafo):
+        n = 0
+        if i == 0:
+            val = len(vizinhos)
+            if i in vizinhos:
+                val -= 1
+                continue
+        if i in vizinhos:
+            n = len(vizinhos) - 1
+        else:
+            n = len(vizinhos)
+        if val != n:
+            return False
+    return True
+        
+
+print('O grafo é regular' if isRegular(grafo) else 'O grafo não é regular')
